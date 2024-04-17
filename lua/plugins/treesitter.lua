fe5126a8aa0afc60fa18a893bd16af5b59836121
KingTingTheGreat <jting@bu.edu>
2024-04-17 08:05:01 -0400

    update ts config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,9 +4,13 @@ return {
     config = function()
 	local config = require("nvim-treesitter.configs")
 	config.setup({
-	    ensure_installed = { "lua", "javascript", "python", "typescript", "go" },
+	    ensure_installed = { "lua", "javascript", "python", "typescript", "go", "bash", "css", "html" },
+	    sync_install = false,
 	    indent = { enable = true },
 	    highlight = { enable = true },
+	    matchup = { enable = true },
+	    auto_install = true,
+	    ignore_install = {},
 	})
     end
 }
