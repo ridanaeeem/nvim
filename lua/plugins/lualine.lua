43209d081b0110eea433aaca72c501d04c7698f7
KingTingTheGreat <jting@bu.edu>
2024-04-17 08:01:36 -0400

    formatting

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,13 +1,13 @@
 return {
     "nvim-lualine/lualine.nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons"
-	},
+    dependencies = {
+	"nvim-tree/nvim-web-devicons"
+    },
     config = function()
-        require "lualine".setup({
-            options = {
-                theme = "ayu_mirage"
-            }
-        })
+	require "lualine".setup({
+	    options = {
+		theme = "ayu_mirage"
+	    }
+	})
     end
 }
