6fd1959de21d2c420655e2c084d35a15fddd7e61
KingTingTheGreat <jting@bu.edu>
2024-04-17 14:15:09 -0400

    vim-sleuth in its own flie

--- init.lua
+++ init.lua
@@ -79,12 +79,6 @@ if not vim.loop.fs_stat(lazypath) then
     })
 end
 vim.opt.rtp:prepend(lazypath)
---[[
-require("lazy").setup({
-	-- "tpope/vim-sleuth",
-	{ "numToStr/Comment.nvim", opts = {} },
-})
---]]
 local opts = {}
 
 require("lazy").setup("plugins", opts)
