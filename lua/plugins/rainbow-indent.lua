83e2d7349a1d2dc722dc880dd3aee71bf662c686
KingTingTheGreat <jting@bu.edu>
2024-04-16 23:12:02 -0400

    adjusting rainbow indent

--- lua/plugins/rainbow-indent.lua
+++ lua/plugins/rainbow-indent.lua
@@ -1,9 +1,3 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
-	opts = function(_, opts)
-		return require("index-rainbowline").make_opts(opts)
-	end,
-	dependencies = {
-		"TheGLander/index-rainbow.nvim",
-	},
 }
